a31766a01ed2bf6a7db78ef4b4b06be8a3d67370
Ben Mahowald <ben.mahowald@gmail.com>
2016-09-26 13:44:32 -0500

    corrected spacing and added some comments

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,28 +1,33 @@
+// pull in angular
 var myApp=angular.module( 'myApp', [] );
+console.log('script src');
+
 myApp.controller( 'whereMyPeeps', [ '$scope', '$http', function( $scope, $http ){
-$scope.addRecord = function(){
-event.preventDefault();
-var objectToSend ={
-name: $scope.nameIn,
-location: $scope.locationIn,
-};
-$http({
-method: 'POST',
-url: '/testPost',
-data: objectToSend
-});
-$scope.nameIn ='';
-$scope.locationIn='';
-};
-$scope.getRecords = function(){
-$.http({
-method: 'GET',
-url: '/getRecords',
-}).then( function( response ){
-$scope.allTheRecords = response;
-console.log( $scope.allTheRecords );
-}), function myError( response ){
-console.log( response.statusText );
-};
-};
+  $scope.addRecord = function(){
+    console.log('in addRecord');
+    event.preventDefault();
+    var objectToSend ={
+      name: $scope.nameIn,
+      location: $scope.locationIn,
+    };
+    $http({
+      method: 'POST',
+      url: '/testPost',
+      data: objectToSend
+    });
+    $scope.nameIn ='';
+    $scope.locationIn='';
+  };
+  $scope.getRecords = function(){
+    console.log('in getRecords');
+    // $.http({
+    //   method: 'GET',
+    //   url: '/getRecords',
+    // }).then( function( response ){
+    //   $scope.allTheRecords = response;
+    //   console.log( $scope.allTheRecords );
+    // }), function myError( response ){
+    //   console.log( response.statusText );
+    // };
+  };
 }]);
